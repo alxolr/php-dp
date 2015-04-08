233492c50ab504bb851939d8cbc2a4b2f7358335
Alexandru Olaru <olarualexandru90@gmail.com>
2015-04-08 09:47:38 +0300

    Added the code example for chain of responsibility

--- gof/behavioral/ChainOfResponsibility.php
+++ gof/behavioral/ChainOfResponsibility.php
@@ -1,4 +1,89 @@
 <?php
-class ChainOfResponsibility {
 
+/**
+ * Class Logger
+ */
+abstract class Logger
+{
+    /**
+     * @var Logger
+     */
+    private $next = null;
+
+    /**
+     * @param Logger $next
+     * @return Logger|null
+     */
+    public function setNext(Logger $next)
+    {
+        $this->next = $next;
+
+        return $this->next;
+    }
+
+    /**
+     * @param $message
+     */
+    public function log($message)
+    {
+        $this->doLog($message);
+        if ($this->next !== null) {
+            $this->next->doLog($message);
+        }
+    }
+
+    /**
+     * @param $message
+     * @return mixed
+     */
+    abstract protected function doLog($message);
 }
+
+/**
+ * Class EmailLogger
+ */
+class EmailLogger extends Logger
+{
+    /**
+     * @param $message
+     * @return mixed|void
+     */
+    protected function doLog($message)
+    {
+        echo "Sending via email: ", $message, " \n";
+    }
+}
+
+/**
+ * Class ErrorLogger
+ */
+class ErrorLogger extends Logger
+{
+    /**
+     * @param $message
+     * @return mixed|void
+     */
+    protected function doLog($message)
+    {
+        echo "Sending to stderr: ", $message, " \n";
+    }
+}
+
+/**
+ * Class StdoutLogger
+ */
+class StdoutLogger extends Logger
+{
+    /**
+     * @param $message
+     * @return mixed|void
+     */
+    protected function doLog($message)
+    {
+        echo "Writing to stdout: ", $message, " \n";
+    }
+}
+
+$logger = new StdoutLogger();
+$logger->setNext(new ErrorLogger())->setNext(new EmailLogger());
+$logger->log('Something happened');
