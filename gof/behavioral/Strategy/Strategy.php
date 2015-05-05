6efbec5104b3ed921b8219d3e9d4c71892f14054
Alexandru Olaru <olarualexandru90@gmail.com>
2015-05-05 14:43:19 +0300

    Added ideas for the strategy code example

--- gof/behavioral/Strategy/Strategy.php
+++ gof/behavioral/Strategy/Strategy.php
@@ -1,1 +1,29 @@
 <?php
+
+abstract class Employee
+{
+    protected $name;
+    protected $age;
+    protected $hourlyRate = 4.65;
+    protected $experience;
+}
+
+class Developer extends Employee
+{
+}
+
+class QualityAnalyst extends Employee
+{
+}
+
+abstract class AbstractSalaryStrategy
+{
+    abstract public function calculateSalary(Employee $employee);
+}
+
+class FullTimePayment extends AbstractSalaryStrategy
+{
+    public function calculateSalary(Employee $employee)
+    {
+    }
+}
